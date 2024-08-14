67c97db65b2bcc2e8d2032730468b5e1b6b7136e
Thanzito <163420206+Thanzito@users.noreply.github.com>
2024-08-14 07:36:52 -0300

    Add files via upload

--- condicionais.js
+++ condicionais.js
@@ -2,6 +2,7 @@ console.log("Trabalhando com condicionais")
 
 const idadeComprador = 15;
 const estaAcompanhada = true;
+const pasagem = false;
 
 const listaDestinos = new Array (
     `Salvador`,//0
@@ -15,13 +16,20 @@ console.log("Destinos possíveis:");
 console.log(listaDestinos);
 
 if (idadeComprador >= 18 || estaAcompanhada == true){
+    passagem = true;
     console.log("Comprador maior de idade!");
     listaDestinos.splice(1,1); //removendo item
 }   else {
     console.log("Não é maior de idade e não posso vender.");
     }
 
-    console.log("Desino possíveis:");
-    console.log(listaDestinos);
+    console.log("Embarque: \n \n");
+
+    if(idadeComprador>=18 || passagem == true){
+        console.log("Boa viagem");
+    } else{
+        console.log("Você não pode embarcar.");
+    }
+  
 
 
---
+++ operacoesAritmeticas.js
@@ -0,0 +1,9 @@
+console.log("Operações Aritméticas");
+//Números
+console.log(2 + 2);
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+//String ou texto
+
+console.log("ano "+2020);
+console.log("2 " + 2)
